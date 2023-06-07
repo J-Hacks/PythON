4ae1bbc1dbc896719119da1d5f332bb538636140
J-Hacks <132538963+J-Hacks@users.noreply.github.com>
2023-06-07 10:30:54 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,22 @@
+def myfunc():
+    for j in range(i, -1, -1):
+        if len(b) < N:
+            if j < N :
+                b.append(a[j])
+            else:
+                continue
+def group():
+    for y in range(1, N+1):
+        x = (y*k) - 1
+        element.append(x)
+N = 6
+a = [1,2,4,3,5,8]
+k = 3
+b = []
+element = []
+group()
+for i in (element):
+    myfunc()
+print(b)
+
+
