b3ed65ba49cf538df893ea95716f82c03efcb830
J-Hacks <132538963+J-Hacks@users.noreply.github.com>
2023-06-08 10:19:51 +0530

    Update sumof_continous_element.py

--- sumof_continous_element.py
+++ sumof_continous_element.py
@@ -3,6 +3,7 @@ def sumof():
         element.append(x)
         if sum(element) == s:
             print(f'{element[0]}  {element[-1]}')
+            return 1
     return -1
 
 a = [2,3,4,5,6,7,8,9,10]
@@ -12,6 +13,7 @@ for y in range(0, (len(a)-1)):
     if sumof() == -1:
         a.pop(0)
         element = []
-        sumof()
+        continue
+    break
 
 
