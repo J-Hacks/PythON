0456b86896c08adfa27d59aa03d97e9b38037881
J-Hacks <132538963+J-Hacks@users.noreply.github.com>
2024-06-09 16:43:21 +0530

    Update TCP_Server.py

--- Black_hat/TCP_Server.py
+++ Black_hat/TCP_Server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 IP = '0.0.0.0'
 PORT = 9998
+
 def main():
     server = socket.socket(socket.AF_INET,
     socket.SOCK_STREAM)
@@ -13,6 +14,7 @@ def main():
         print(f'[*] Accepted connection from {address[0]}:{address[1]}')
         client_handler = threading.Thread(target=handle_client, args=(client,))
         client_handler.start() 
+        
 def handle_client(client_socket): 
     with client_socket as sock:
         request = sock.recv(1024)
