02ded7cc7fbf87302732878ad2efc597f979bd49
J-Hacks <132538963+J-Hacks@users.noreply.github.com>
2024-06-11 07:53:31 +0530

    get code from SVN

---
+++ DEVOPS/SVN_CHECKOUT.py
@@ -0,0 +1,24 @@
+import subprocess
+ 
+def svn_checkout(repo_url, destination, username, password):
+
+    try:
+
+        subprocess.run(["svn", "checkout", repo_url, destination, "--username", username, "--password", password])
+
+        print("Checkout successful!")
+
+    except subprocess.CalledProcessError as e:
+
+        print("Error:", e)
+ 
+
+repo_url = "https://svn.example.com/svn/project/trunk"
+
+destination = "/path/to/checkout/directory"
+
+username = "your_username"
+
+password = "your_password"
+
+svn_checkout(repo_url, destination, username, password)
